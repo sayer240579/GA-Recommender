71a9743be49d5d14875bb8f74c7586312464403f
sayer240579 <sayralahmr51@gmail.com>
2026-04-22 04:30:03 +0200

    app.py

--- GA_Recommender_System/app.py
+++ GA_Recommender_System/app.py
@@ -1,10 +1,9 @@
-
+from flask import Flask, render_template, request, redirect
 from backend.data_loader import load_all_data, update_behavior
 from backend.recommender import RecommenderSystem
 import pandas as pd
 
-
-
+app = Flask(__name__)
 users, products, ratings, behavior = load_all_data("data")
 rec_system = RecommenderSystem(products, behavior, ratings)
 
